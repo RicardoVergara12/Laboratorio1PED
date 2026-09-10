c798ae7298f173eaa3e07d10f501282af7e6625f
LennyStevensss <00093526@uca.edu.sv>
2026-09-10 15:06:56 -0600

    :memo:retirar unicamente multiplos de 5

--- Retirar-con-paso-de-puntero.cpp
+++ Retirar-con-paso-de-puntero.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
-float retirar;
-float* pRetirar = &retirar;
+int retirar;
+int* pRetirar = &retirar;
 
 void retirardinero() {
     std::cout << "Ingrese la cantidad a retirar: ";
     std::cin >> *pRetirar;
 
-    if (*pRetirar / 5 == 0&&5 ){
-        std::cout << "el dinero retirado es de: " << *pRetirar << std::endl;
-    }
-    else if (*pRetirar / 5 == 0&&5) {
-        std::cout << "el dinero no puede ser retirado por que no es multiplo de 5" << std::endl;
-    }
+    int residuo;
+    int* pResiduo = &residuo;
 
+    *pResiduo = *pRetirar % 5;
+
+    if (*pResiduo == 0) {
+        std::cout << "El dinero retirado es de: " << *pRetirar << std::endl;
+    }
+    else {
+        std::cout << "El dinero no puede ser retirado porque no es multiplo de 5" << std::endl;
+    }
 }
-
-
 
 int main() {
     retirardinero();
